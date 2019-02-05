566e889bf1957a51f4dadc8f3d35df5febb73f40
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-02-05 22:42:09 +0300

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const folderController = require('../controllers/folderController');
-// const storeController = require('../controllers/storeController');
-// const userController = require('../controllers/userController');
-// const authController = require('../controllers/authController');
-// const reviewController = require('../controllers/reviewController');
 const {catchErrors} = require('../handlers/errorHandlers');
 
 /* API */
@@ -13,11 +9,5 @@ router.get('/api/folder/:id', catchErrors(folderController.get));
 router.post('/api/folder', catchErrors(folderController.create));
 // router.put('/api/folder/:id', updateFolderController);
 // router.delete('/api/folder/:id', deleteFolderController);
-
-
-
-// router.get('/api/search', catchErrors(storeController.searchStores));
-// router.get('/api/stores/near', catchErrors(storeController.mapStores));
-// router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 module.exports = router;
