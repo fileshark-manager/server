f0455a3eb3c7dce0c81324219f30c85b0b34b88c
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-03-23 13:17:04 +0300

    Update routes comments

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,15 @@ const folderController = require('../controllers/folderController');
 const fileController = require('../controllers/fileController');
 const {catchErrors} = require('../handlers/errorHandlers');
 
-/* API */
+/* API FOR FOLDERS */
 router.get('/api/folder', catchErrors(folderController.getAll));
 router.get('/api/folder/:id', catchErrors(folderController.get));
 router.post('/api/folder', catchErrors(folderController.create));
 router.put('/api/folder/:id', catchErrors(folderController.update));
 router.delete('/api/folder/:id', catchErrors(folderController.delete));
 
-router.get('/api/file', catchErrors(fileController.getAll)); // учесть offset, limit, folder в GET параметрах для пагинации
+/* API FOR FILES */
+router.get('/api/file', catchErrors(fileController.getAll));
 router.get('/api/file/:id', catchErrors(fileController.get));
 router.post('/api/file',
     fileController.upload,
@@ -21,6 +22,7 @@ router.post('/api/file',
 router.put('/api/file/:id', catchErrors(fileController.update));
 router.delete('/api/file/:id', catchErrors(fileController.delete));
 
+/* API FOR DEMO PURPOSE ONLY (RESETS AND CREATES SAMPLE DATA) */
 router.get('/api/bigbang',
     catchErrors(folderController.bang),
     catchErrors(fileController.bang),
